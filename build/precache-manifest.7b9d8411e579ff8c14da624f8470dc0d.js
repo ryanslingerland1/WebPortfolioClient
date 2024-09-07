ad4ca83f4969cf023949045b4e7dc92e58875a19
cv22am <cv22am@brocku.ca>
2024-09-06 23:00:04 -0400

    changes

---
+++ build/precache-manifest.7b9d8411e579ff8c14da624f8470dc0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d78736f9fed0322fb83e798930d6ef5",
+    "url": "/WebPortfolioClient/index.html"
+  },
+  {
+    "revision": "258235242954e0f0ea52",
+    "url": "/WebPortfolioClient/static/css/main.87364ba6.chunk.css"
+  },
+  {
+    "revision": "258235242954e0f0ea52",
+    "url": "/WebPortfolioClient/static/js/main.c218f1fc.chunk.js"
+  },
+  {
+    "revision": "93cfbcdadb303470fbf4",
+    "url": "/WebPortfolioClient/static/js/runtime~main.b47579ac.js"
+  },
+  {
+    "revision": "ee87f9c0fa14b01b212c68c640de7e4b",
+    "url": "/WebPortfolioClient/static/media/Portrait-23.ee87f9c0.png"
+  }
+]);
